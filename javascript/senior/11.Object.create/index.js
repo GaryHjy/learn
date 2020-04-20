863fdac17b611f71e04a9fc769df83736880ced8
GaryHjy <782316959@qq.com>
2020-04-20 23:51:48 +0800

    feat: 添加实现Object.create()

---
+++ javascript/senior/11.Object.create/index.js
@@ -0,0 +1,19 @@
+let obj = {
+  name: '小明',
+  age: 18
+}
+
+// Object.create()方法创建一个新对象，使用现有的对象来提供新创建的对象的__proto__  // MDN描述
+let newObj = Object.create(obj)
+
+console.log(obj, newObj.__proto__)
+
+function createObj(proto) {
+  function F() {}
+  F.prototype = proto // 将对象继承为prototype属性
+  return new F;
+}
+
+let mockCreate = createObj(obj);
+
+console.log(obj, mockCreate.__proto__);
