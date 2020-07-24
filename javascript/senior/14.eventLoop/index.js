1d73e68d19d30c2a05534a8528dd2e91b413072e
GaryHjy <782316959@qq.com>
2020-07-24 22:21:14 +0800

    feat: update

--- javascript/senior/14.eventLoop/index.js
+++ javascript/senior/14.eventLoop/index.js
@@ -1,2 +1,21 @@
 // Tasks, microTasks, queues and schedules
 // 事件循环
+
+// macroTasks(宏任务): setTimeout, setInterval, setImmediate, I/O, UI rendering
+// microTasks(微任务): process.nextTick, Promise, MutationObserver
+
+console.log('script start');
+
+setTimeout(function () {
+  console.log('setTimeout');
+}, 0);
+
+Promise.resolve()
+  .then(function () {
+    console.log('promise1');
+  })
+  .then(function () {
+    console.log('promise2');
+  });
+
+console.log('script end');
