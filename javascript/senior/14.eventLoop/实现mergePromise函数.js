4dc1c1380eb42af344e17630dd639f3e3992cc9a
GaryHjy <782316959@qq.com>
2020-08-11 14:31:47 +0800

    feat: 添加

---
+++ javascript/senior/14.eventLoop/实现mergePromise函数.js
@@ -0,0 +1,34 @@
+const time = (timer) => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve()
+    }, timer)
+  })
+}
+const ajax1 = () => time(2000).then(() => {
+  console.log(1);
+  return 1
+})
+const ajax2 = () => time(1000).then(() => {
+  console.log(2);
+  return 2
+})
+const ajax3 = () => time(1000).then(() => {
+  console.log(3);
+  return 3
+})
+
+function mergePromise (arr) {
+  const data = []
+  return arr.reduce((p, item) => {
+    return p.then(item).then(res => {
+      data.push(res)
+      return data
+    })
+  }, Promise.resolve())
+}
+
+mergePromise([ajax1, ajax2, ajax3]).then(data => {
+  console.log("done");
+  console.log(data); // data 为 [1, 2, 3]
+});
