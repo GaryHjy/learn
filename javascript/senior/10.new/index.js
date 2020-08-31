0436f08e839a3bb2dc0314f54012c9030d8a086e
GaryHjy <782316959@qq.com>
2020-08-31 20:45:10 +0800

    feat: 修改

--- javascript/senior/10.new/index.js
+++ javascript/senior/10.new/index.js
@@ -23,7 +23,7 @@ console.log(animal)
 // 它会被执行[[Prototype]]（也就是__proto__）链接
 // 它使this指向新创建的对象
 // 通过new创建的每个对象将最终被[[Prototype]]链接到这个函数的prototype对象上
-// 如果函数没有返回对象类型Object(包含Functoin, Array, Date, RegExg, Error) ，那么new表达式中的函数调用将返回该对象引用
+// 如果函数没有返回对象类型Object(包含Function, Array, Date, RegExg, Error) ，那么new表达式中的函数调用将返回该对象引用
 
 function mockNew() {
   // 截取arguments的第一个值，Constructor
