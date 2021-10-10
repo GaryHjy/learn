aa272bb8d5ef48c93fd0f7670a69d7ebac94c341
GaryHjy <782316959@qq.com>
2021-10-10 19:19:26 +0800

    feat: 超时可终止的promise

---
+++ 编程题/超时可终止的promise.js
@@ -0,0 +1,18 @@
+function timeout(fn, time) {
+}
+
+function test(name) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(name)
+    }, 500)
+  })
+}
+
+let newTest = timeout(test, 500)
+
+newTest('aaa').then(res => {
+  console.log(res);
+}).catch(e => {
+  console.log(e);
+})
