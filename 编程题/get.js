4c1dc2b342e9084bbebe4b42e410e7e39d44dfeb
GaryHjy <782316959@qq.com>
2021-11-06 18:19:05 +0800

    feat: 新增编程题

---
+++ 编程题/get.js
@@ -0,0 +1,7 @@
+// 按照如下案例,写出get函数的具体实现逻辑:
+// get('a.b.c',{a:{b:{c:1}}}) // output: 1
+// get('a.b.c',{a:{b:{}}}) // output: undefined
+// get('a.b.__proto__',{a:{b:{}}}) // output: undefined
+// get('a.b.c',{a:{b:{c:0}}}) // output: 0
+// get('a.b',{a:{b:null}}) // output: null
+// get('a.b.c',{a:{b:[1,2,3]}}) //output: [1,2,3]
---
+++ 编程题/生成SKU.js
@@ -0,0 +1,29 @@
+// 生成SKU
+// 已知规格数据
+const spu = 'AB1234567';
+const specList = [ 
+  ["red", "yellow"],
+  ["XL", "S"], 
+  ['a1', 'a2'],
+  ['b1', 'b2'],
+];
+/** 
+  输出结果：
+  AB1234567-red-XL-a1-b1;
+  AB1234567-red-XL-a1-b2;
+  AB1234567-red-XL-a2-b1;
+  AB1234567-red-XL-a2-b2;
+  AB1234567-red-S-a1-b1;
+  AB1234567-red-S-a1-b2;
+  AB1234567-red-S-a2-b1;
+  AB1234567-red-S-a2-b2;
+  AB1234567-yellow-XL-a1-b1;
+  AB1234567-yellow-XL-a1-b2;
+  AB1234567-yellow-XL-a2-b1;
+  AB1234567-yellow-XL-a2-b2;
+  AB1234567-yellow-S-a1-b1;
+  AB1234567-yellow-S-a1-b2;
+  AB1234567-yellow-S-a2-b1;
+  AB1234567-yellow-S-a2-b2;
+  ....
+*/
