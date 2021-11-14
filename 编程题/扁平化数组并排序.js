272e4d129c8c03aa699b89a2833752f2792f7395
GaryHjy <782316959@qq.com>
2021-11-14 11:10:25 +0800

    feat: 修改

--- 编程题/扁平化数组并排序.js
+++ 编程题/扁平化数组并排序.js
@@ -2,14 +2,7 @@
 const arr = [3,[11,[5,6],7,9,1,2,4],23,[5,6,12],31,2]
 
 const flatArr = function(arr) {
-  return arr.reduce((result, item) => {
-    if (Array.isArray(item)) {
-      result = result.concat(flatArr(item))
-    } else {
-      result.push(item)
-    }
-    return result
-  }, [])
+  return arr.reduce((result, item) => result.concat(Array.isArray(item) ? flatArr(item) : [item]), [])
 }
 
 const resultArr = [...new Set(flatArr(arr))].sort((a, b) => a - b)
