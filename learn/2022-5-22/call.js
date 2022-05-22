13ee10efe845731fbee8cde223a9364f4a60b3c6
GaryHjy <782316959@qq.com>
2022-05-23 00:44:20 +0800

    feat: 每日一练

---
+++ learn/2022-5-22/call.js
@@ -0,0 +1,26 @@
+
+Function.prototype.myCall = function(context) {
+  context = Object.create(context) || window;
+
+  context.fn = this;
+
+  var args = [];
+  for(var i = 1; i < arguments.length; i++) {
+    args.push('arguments[' + i + ']');
+  }
+
+  var result = eval('context.fn(' + args + ')');
+
+  delete context.fn;
+  return result;
+}
+
+var obj = {
+  name: 'obj name'
+}
+
+function showName(name, age) {
+  console.log(this.name, name, age);
+}
+
+showName.myCall(obj, 'xiaoming', 18);
