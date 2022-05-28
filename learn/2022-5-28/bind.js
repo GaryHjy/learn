4902c5a3cf81f78fae2619c8ac084996527cf3fd
GaryHjy <782316959@qq.com>
2022-05-29 00:01:30 +0800

    feat: 每日一练

---
+++ learn/2022-5-28/bind.js
@@ -0,0 +1,26 @@
+Function.prototype.myBind = function(context) {
+  var self = this;
+  var args = Array.prototype.slice.call(arguments, 1);
+
+  function Fn() {};
+  Fn.prototype = this.prototype;
+
+  function bound() {
+    var _args = args.concat(Array.prototype.slice.call(arguments));
+    return self.apply(this instanceof Fn ? this : context, _args);
+  }
+
+  bound.prototype = new Fn();
+  return bound
+}
+
+
+var obj = {
+  name: 'obj name'
+}
+
+function showName(name, age) {
+  console.log(name, age, this.name);
+}
+
+showName.myBind(obj, 'xiaoming')(18);
---
+++ learn/2022-5-28/curry.js
@@ -0,0 +1,20 @@
+function curry(fn) {
+  var length = fn.length;
+  var self = this;
+  var args = Array.prototype.slice.call(arguments, 1);
+
+  return function() {
+    var _args = args.concat(Array.prototype.slice.call(arguments));
+    if (length > _args.length) {
+      return curry.apply(self, [fn].concat(_args));
+    } else {
+      return fn.apply(this, _args);
+    }
+  }
+}
+
+var fn = curry(function(a, b, c, d, e) {
+  console.log([a, b, c, d, e]);
+})
+
+fn(1)(2, 3)(4)(5);
