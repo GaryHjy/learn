fa099a3a43ef8bf8ee888eeba875e354acddd56b
GaryHjy <782316959@qq.com>
2022-06-19 20:31:48 +0800

    feat: 每日一练

---
+++ learn/2022-6-19/apply.js
@@ -0,0 +1,27 @@
+Function.prototype.myApply = function(context, arr) {
+  context = Object.create(context) || window;
+  context.fn = this;
+
+  var result
+  if(!arr) {
+    result = context.fn();
+  } else {
+    var args = [];
+    for(var i = 0; i < arr.length; i++) {
+      args.push('arr[' + i + ']');
+    }
+    result = eval('context.fn(' + args + ')');
+  }
+  delete context.fn;
+  return result;
+}
+
+var obj = {
+  name: 'obj name'
+}
+
+function showName(name, age) {
+  console.log(name, age, this.name);
+}
+
+showName.myApply(obj, ['xiaoming', 18]);
