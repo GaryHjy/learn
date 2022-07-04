3a63be771c4eeb5f9c4df984010e25f4b547ace7
GaryHjy <782316959@qq.com>
2022-07-04 17:21:15 +0800

    feat: 每日一练

---
+++ learn/2022-7-4/防抖debounce.js
@@ -0,0 +1,16 @@
+function debounce(fn, wait = 500) {
+  let timer;
+  return function(...args) {
+    if(timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.apply(this, args)
+    }, wait);
+  }
+}
+
+const fn = debounce(function(name) {
+  console.log('name:', name); 
+})
+
+fn('xiaoming');
+fn('xiaohong')
