a2c861a7cfe0933de2725544782769b6ddc65512
GaryHjy <782316959@qq.com>
2022-07-07 00:06:41 +0800

    feat: 每日一练

---
+++ learn/2022-7-6/数组项出现次数最多的数.js
@@ -0,0 +1,17 @@
+function toMaximum(arr) {
+  let count = 0;
+  let value;
+
+  arr.reduce((map, key) => {
+    map[key] = map[key] ? map[key] += 1 : 1;
+    if(map[key] > count) {
+      count = map[key];
+      value = key;
+    }
+    return map;
+  }, {})
+  return value;
+}
+
+const key = toMaximum([1, 2, 3, 4, 5, 5, 6])
+console.log(key);
