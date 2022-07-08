87e1b50b23cd123d577fd603b372bcc8655ed70a
GaryHjy <782316959@qq.com>
2022-07-08 23:30:01 +0800

    feat: 每日一练

---
+++ learn/2022-7-8/12.超时可终止的promise.js
@@ -0,0 +1,27 @@
+function timeout(fn, time) {
+  const timeoutFn = new Promise((resolve) => {
+    setTimeout(resolve, time)
+  }).then(() => {
+    throw new Error('timeout')
+  })
+  
+  return function(...args) {
+    return Promise.race([fn.apply(this, args), timeoutFn])
+  }
+}
+
+function test(name) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(name)
+    }, 500)
+  })
+}
+
+let newTest = timeout(test, 500)
+
+newTest('aaa').then(res => {
+  console.log(res);
+}).catch(e => {
+  console.log(e);
+})
---
+++ learn/2022-7-8/6.节流throttle.js
@@ -0,0 +1,33 @@
+function throttle(fn, wait = 500) {
+  let last = 0;
+
+  return function(...args) {
+    const now = Date.now();
+    if(now - last >= wait) {
+      last = now;
+      fn.apply(this, args);
+    }
+  }
+}
+
+function throttle(fn, wait = 500) {
+  let timer = null;
+  
+  return function(...args) {
+    if(!timer) {
+      timer = setTimeout(() => {
+        fn.apply(this, args);
+        timer = null;
+      }, wait);
+    }
+  }
+}
+
+
+const fn  = throttle(function(name) {
+  console.log('name:', name);
+})
+
+
+fn('xiaoming');
+fn('xiaohong');
