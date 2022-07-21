f4425013be094f3098decd856eb9da6e30c2068f
GaryHjy <782316959@qq.com>
2022-07-21 23:47:34 +0800

    feat: 每日一练

---
+++ learn/2022-7-21/6.节流throttle.js
@@ -0,0 +1,31 @@
+function throttle(fn, wait = 500) {
+  let last = 0;
+  return function(...args) {
+    const now = Date.now();
+    if(now - last >= wait) {
+      fn.apply(this, args);
+      last = now;
+    }
+  }
+}
+
+function throttle(fn, wait = 500) {
+  let timer = null;
+  return function(...args) {
+    if(!timer) {
+      timer = setTimeout(() => {
+        fn.apply(this, args);
+        timer = null;
+      }, wait);
+    }
+  }
+}
+
+
+const fn  = throttle(function(name) {
+  console.log('name:', name);
+})
+
+
+fn('xiaoming');
+fn('xiaohong');
